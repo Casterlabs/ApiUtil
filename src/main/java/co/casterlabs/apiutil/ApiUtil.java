12ea2ba633bc76a0c0a22a55f0929a7ce1096fd3
Ender <33337309+e3ndr@users.noreply.github.com>
2020-12-10 02:32:33 -0600

    Add an error reporting class.

--- src/main/java/co/casterlabs/apiutil/ApiUtil.java
+++ src/main/java/co/casterlabs/apiutil/ApiUtil.java
@@ -1,7 +1,5 @@
 package co.casterlabs.apiutil;
 
-import java.util.Map;
-
 import org.jetbrains.annotations.Nullable;
 
 import lombok.Getter;
@@ -12,7 +10,7 @@ public class ApiUtil {
 
     private static @NonNull @Getter @Setter ErrorReporter errorReporter = new ErrorReporter() {
         @Override
-        public void apiError(@NonNull String url, @Nullable String sentBody, @Nullable Map<Object, Object> sentHeaders, @Nullable String recBody, @Nullable Map<Object, Object> recHeaders, @NonNull Throwable t) {}
+        public void apiError(@NonNull String url, @Nullable String sentBody, @Nullable Object sentHeaders, @Nullable String recBody, @Nullable Object recHeaders, @NonNull Throwable t) {}
     };
 
 }
--- src/main/java/co/casterlabs/apiutil/ErrorReporter.java
+++ src/main/java/co/casterlabs/apiutil/ErrorReporter.java
@@ -1,13 +1,11 @@
 package co.casterlabs.apiutil;
 
-import java.util.Map;
-
 import org.jetbrains.annotations.Nullable;
 
 import lombok.NonNull;
 
 public interface ErrorReporter {
 
-    public void apiError(@NonNull String url, @Nullable String sentBody, @Nullable Map<Object, Object> sentHeaders, @Nullable String recBody, @Nullable Map<Object, Object> recHeaders, @NonNull Throwable t);
+    public void apiError(@NonNull String url, @Nullable String sentBody, @Nullable Object sentHeaders, @Nullable String recBody, @Nullable Object recHeaders, @NonNull Throwable t);
 
 }
