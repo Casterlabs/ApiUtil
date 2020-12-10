e9e706e276d31fbd32faba1d1c4402b922ebf6be
Ender <33337309+e3ndr@users.noreply.github.com>
2020-12-10 02:28:30 -0600

    Add an error reporting class.

--- src/main/java/co/casterlabs/apiutil/ErrorReporter.java
+++ src/main/java/co/casterlabs/apiutil/ErrorReporter.java
@@ -8,6 +8,6 @@ import lombok.NonNull;
 
 public interface ErrorReporter {
 
-    public void apiError(@NonNull String url, @Nullable String sentBody, @Nullable Map<String, String> sentHeaders, @Nullable String recBody, @Nullable Map<String, String> recHeaders, @NonNull Throwable t);
+    public void apiError(@NonNull String url, @Nullable String sentBody, @Nullable Map<Object, Object> sentHeaders, @Nullable String recBody, @Nullable Map<Object, Object> recHeaders, @NonNull Throwable t);
 
 }
